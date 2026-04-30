74c6941ea50d3d57cffd42dc2420c45940b1fed7
‘posuza’ <posu0009@gmail.com>
2026-04-30 16:47:29 +0700

    new commot

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_URL = "http://127.0.0.1:8000/api/v1";
+export const API_URL = "https://small-trains-end.loca.lt/api/v1";
 console.log("API URL:", API_URL);
 
 export const API_CONFIG = {
